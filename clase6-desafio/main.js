9a37ff7b56aba2b6f54ce91cd1d00d8f00e50aba
mariaflorenciaferreira <florferreirag@gmail.com>
2022-07-25 23:08:55 -0300

    desafio clase6 corregido

--- clase6-desafio/main.js
+++ clase6-desafio/main.js
@@ -6,35 +6,39 @@ const app=express()
 const producto= new Contenedor('./listadoProductos.txt')
 const PORT=8080
 
-const server=app.listen(PORT,()=>{
-    console.log(`Escuchando en el puerto: ${server.address().port}`)
 
-})
-server.on('error',(err)=>{ console.log(err) })
+app.get('/',async  (req,res)=>{
 
 
-app.get('/productos', (req,res)=>{
-
-    const productosDisponibles=producto.getAll()
-    res.send(`${productosDisponibles}` )
+    res.send("Bienvenido, para ver los productos ingresar a /productos, para ver un producto al azar ingresar /productoRandom")
 })
 
 
+app.get('/productos',async  (req,res)=>{
+
+    const productosDisponibles= await producto.getAll()
+    res.json(productosDisponibles)
+})
 
 
-
-app.get('/productoRandom', (req,res)=>{
+app.get('/productoRandom', async (req,res)=>{
 
     const numero=(min,max)=>{
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
     
-    console.log(numero)
+    console.log(numero(1,3))
     
-    const productoElegido=producto.getProduct(numero)
-    res.send(`${productoElegido}`)
+    const productoElegido=await producto.getProduct(numero(1,3))
+    res.send(productoElegido)
     console.log(productoElegido)
 })
 
 
+
+const server=app.listen(PORT,()=>{
+    console.log(`Escuchando en el puerto: ${server.address().port}`)
+
+})
+server.on('error',(err)=>{ console.log(err) })
 
